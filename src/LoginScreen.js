2c0f36963c996f05590979d726ab806bd271843f
Ahmed Makram Alsaidy <112633127+ahmed-makram-alsaidy@users.noreply.github.com>
2025-08-30 05:16:17 +0300

    Update LoginScreen.js

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,5 +1,5 @@
 // src/LoginScreen.js
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { login } from './api';
 
 function LoginScreen({ onLoginSuccess }) {
