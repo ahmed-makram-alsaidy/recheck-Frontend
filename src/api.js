f927f606c2c4f048cb0639db804e04f2915af045
Ahmed Makram Alsaidy <112633127+ahmed-makram-alsaidy@users.noreply.github.com>
2025-08-30 05:10:03 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://recheck-backend.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_URL,
